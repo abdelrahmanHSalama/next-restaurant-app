44f30a6eba125f1757c743249fe0e0365a8f2a28
Abdelrahman <AbdelrahmanHelmySalama@outlook.com>
2025-08-12 09:31:00 +0300

    (feat)files-translations

--- src/components/dashboard/Dashboard.tsx
+++ src/components/dashboard/Dashboard.tsx
@@ -60,7 +60,7 @@ const KPIData: KPIDataType[] = [
 const Dashboard = () => {
   return (
     <>
-      <PageTitle title="dashboard" />
+      <PageTitle set="dashboard" />
       <section>
         <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
           {KPIData.map((item) => (
---
+++ src/components/files/Files.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { PageTitle } from '@/components/ui';
+
+const FilesPage = () => {
+  return (
+    <div>
+      <PageTitle set="files" />
+    </div>
+  );
+};
+
+export default FilesPage;
