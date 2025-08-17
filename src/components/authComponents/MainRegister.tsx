eb9a22487d6d252d7afe778cf13f8b236d085c19
Abdelrahman <AbdelrahmanHelmySalama@outlook.com>
2025-08-17 10:44:35 +0300

    auth-design+login-register-server
    *Changed login/register pages design.
    * Stored access token in cookies to avoid checking for the token on the client side.

--- src/components/authComponents/MainRegister.tsx
+++ src/components/authComponents/MainRegister.tsx
@@ -1,9 +1,11 @@
 'use client';
 import { supabase } from '@/services/libs/auth';
 import { GoogleOutlined } from '@ant-design/icons';
-import { Button, Form, Input } from 'antd';
+import { Button, Checkbox, Form, Input } from 'antd';
 import { useTranslations } from 'next-intl';
+import Link from 'next/link';
 import { useRouter } from 'next/navigation';
+import Cookies from 'js-cookie';
 
 type FieldType = {
   email?: string;
@@ -36,6 +38,11 @@ const MainRegister = () => {
       console.error('Error signing up:', error.message);
       return;
     }
+    const accessToken = await supabase.auth.getSession();
+    const token = accessToken.data.session?.access_token;
+    if (token) {
+      Cookies.set('access-token', token, { sameSite: 'lax' });
+    }
     router.replace('/dashboard');
   };
   const handleSignUpWithGoogle = async () => {
@@ -53,38 +60,7 @@ const MainRegister = () => {
   };
   return (
     <>
-      <h1 className="text-4xl font-bold mb-4">{t('title')}</h1>
-      <p className="text-lg text-text/75 mb-10">{t('subtitle')}</p>
-      <Form
-        form={form}
-        name="register"
-        initialValues={{ remember: true }}
-        onFinish={handleSignUp}
-        autoComplete="off"
-        layout="vertical"
-        className="w-full"
-      >
-        <Form.Item
-          label="Email Address"
-          name="email"
-          rules={[{ required: true, message: 'Please enter your email!' }]}
-        >
-          <Input placeholder="Enter your Email Address." />
-        </Form.Item>
-        <Form.Item
-          label="Username"
-          name="username"
-          rules={[{ required: true, message: 'Please enter your username' }]}
-        >
-          <Input placeholder="Enter your your username." />
-        </Form.Item>
-        <Form.Item
-          label="Password"
-          name="password"
-          rules={[{ required: true, message: 'Please enter your password' }]}
-        >
-          <Input.Password placeholder="Enter your your password." />
-        </Form.Item>
+      {/* <h1 className="text-4xl font-bold mb-4">{t('title')}</h1>
         <Form.Item label={null}>
           <Button type="primary" htmlType="submit">
             {t('registerBtn')}
@@ -105,7 +81,111 @@ const MainRegister = () => {
         <span className="text-primary cursor-pointer" onClick={() => router.push('/login')}>
           {t('login')}
         </span>
-      </p>
+      </p> */}
+      <div className="w-[1440px] flex items-center justify-center ">
+        <div className="w-[43.75%] h-[68%] bg-card rounded-2xl flex items-center">
+          <div className="flex justify-between items-center flex-col w-full space-y-2">
+            <div className="text-center">
+              <h1 className="text-[2rem] font-bold font-main">{t('title')}</h1>
+              <h2 className="text-lg text-text">{t('subtitle')}</h2>
+            </div>
+            <Form
+              form={form}
+              name="register"
+              initialValues={{ remember: true }}
+              onFinish={handleSignUp}
+              className="w-full flex flex-col justify-center items-center"
+            >
+              <Form.Item
+                className="w-[82%]"
+                name="email"
+                rules={[
+                  { required: true, message: 'Please enter your email!' },
+                  { type: 'email', message: 'Please enter a valid email' },
+                ]}
+              >
+                <div className="space-y-1">
+                  <h3 className="text-text font-semibold">{t('email')}:</h3>
+                  <Input
+                    className="w-full"
+                    style={{
+                      backgroundColor: 'var(--color-auth-input)',
+                      height: 56,
+                    }}
+                    type="email"
+                    autoComplete="email"
+                    placeholder="user@example.com"
+                  ></Input>
+                </div>
+              </Form.Item>
+              <Form.Item
+                className="w-[82%]"
+                name="username"
+                rules={[{ required: true, message: 'Please enter your username!' }]}
+              >
+                <div className="space-y-1">
+                  <h3 className="text-text font-semibold">{t('username')}:</h3>
+                  <Input
+                    className="w-full"
+                    style={{
+                      backgroundColor: 'var(--color-auth-input)',
+                      height: 56,
+                    }}
+                    placeholder="User"
+                  ></Input>
+                </div>
+              </Form.Item>
+              <Form.Item
+                className="w-[82%]"
+                name="password"
+                rules={[{ required: true, message: 'Please enter your password' }]}
+              >
+                <div className="space-y-1">
+                  <h3 className="text-text font-semibold">{t('password')}:</h3>
+                  <Input.Password
+                    className="w-full"
+                    style={{
+                      backgroundColor: 'var(--color-auth-input)',
+                      height: 56,
+                    }}
+                    autoComplete="current-password"
+                    placeholder="••••••••"
+                  ></Input.Password>
+                </div>
+              </Form.Item>
+              <Button
+                className="w-[66%] font-bold"
+                style={{
+                  backgroundColor: 'var(--color-primary)',
+                  color: 'white',
+                  fontWeight: 700,
+                  fontSize: 16,
+                  height: 56,
+                  marginTop: 16,
+                }}
+                htmlType="submit"
+              >
+                {t('loginBtn')}
+              </Button>
+              <Button
+                type="link"
+                icon={<GoogleOutlined />}
+                className="w-[82%]"
+                size="large"
+                onClick={handleSignUpWithGoogle}
+              >
+                {t('googleBtn')}
+              </Button>
+              <p className="font-semibold mt-2">
+                {t('haveAcc')}{' '}
+                <Link href="" className="text-primary underline">
+                  {t('login')}
+                </Link>
+              </p>
+            </Form>
+          </div>
+        </div>
+      </div>
     </>
   );
 };
